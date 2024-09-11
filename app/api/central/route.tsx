7506eb1c0e21edcdf5a039cb0e759eaff5f2f1f0
randiandrio <randiandrio@gmail.com>
2024-09-12 02:49:11 +0700

    simpan absen

--- app/api/central/route.tsx
+++ app/api/central/route.tsx
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import moment from "moment";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
@@ -8,3 +9,19 @@ export const POST = async (request: NextRequest) => {
   console.log(body.CustomizeID);
   return NextResponse.json(true, { status: 200 });
 };
+
+async function Post(data: any) {
+  const user = await prisma.user.findUnique({
+    where: { id: data.CustomizeID },
+  });
+  await prisma.presensi.create({
+    data: {
+      sekolahId: Number(user?.sekolahId),
+      tanggal: moment().format("YYYY-MM-DD"),
+      status: "Hadir",
+      userId: Number(user?.id),
+    },
+  });
+
+  return true;
+}
