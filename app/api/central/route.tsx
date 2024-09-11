135500dd32bef8126501e694315839a4249fac91
randiandrio <randiandrio@gmail.com>
2024-09-12 03:01:27 +0700

    adfasfe

--- app/api/central/route.tsx
+++ app/api/central/route.tsx
@@ -7,6 +7,7 @@ const prisma = new PrismaClient();
 export const POST = async (request: NextRequest) => {
   const body: any = await request.json();
 
+  console.log(body);
   return NextResponse.json(body, { status: 200 });
 };
 
