62537ea6ba1f5ea030a31cb76848d7bb1973e435
randiandrio <randiandrio@gmail.com>
2024-09-12 03:08:21 +0700

    sdfgsgrse

--- app/api/central/route.tsx
+++ app/api/central/route.tsx
@@ -8,7 +8,7 @@ export const POST = async (request: NextRequest) => {
   const body: any = await request.json();
   const result = await Post(body);
 
-  console.log(result?.nama);
+  console.log(result);
   return NextResponse.json(true, { status: 200 });
 };
 
@@ -16,14 +16,14 @@ async function Post(data: any) {
   const user = await prisma.user.findUnique({
     where: { id: data.CustomizeID },
   });
-  //   const response = await prisma.presensi.create({
-  //     data: {
-  //       sekolahId: Number(user?.sekolahId),
-  //       tanggal: moment().format("YYYY-MM-DD"),
-  //       status: "Hadir",
-  //       userId: Number(user?.id),
-  //     },
-  //   });
+  const response = await prisma.presensi.create({
+    data: {
+      sekolahId: Number(user?.sekolahId),
+      tanggal: moment().format("YYYY-MM-DD"),
+      status: "Hadir",
+      userId: Number(user?.id),
+    },
+  });
 
-  return user;
+  return response;
 }
